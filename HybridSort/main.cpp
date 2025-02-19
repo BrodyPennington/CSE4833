4acfff4f1e72484489958c432f3f1c0456bf9380
Brody Pennington <Brodygpennington@gmail.com>
2025-02-19 01:24:28 -0600

    hybridSort

---
+++ HybridSort/main.cpp
@@ -0,0 +1,77 @@
+#include "SortFunctions.h"
+#include "arrGen.h"
+#include "chrono.h"
+#include <iostream>
+#include <fstream>
+
+
+
+
+int main() {
+    Chrono timer; // measures time
+
+    std::vector<int> sizes;
+    std::vector<double> selectionTimes;
+    std::vector<double> mergeTimes;
+
+
+    bool foundCrossover = false;
+    int crossoverSize = -1; 
+
+    std::ofstream outFile("results.csv");
+    if (!outFile.is_open()) {
+        std::cerr << "Error: Could not open results.csv for writing.\n";
+        return 1;
+    }
+
+    outFile << "ArraySize,SelectionSort,MergeSort\n";
+
+    std::cout << "Array size, selectionSort (ms), mergeSort (ms)" << std::endl;
+
+    for (int size = 10; size <= 10000; size += 10) {
+        
+        // generate an array
+        std::vector<int> baseArray = generateRandomArray(size, 1, 200);
+
+        // make a copy for selection sort
+        std::vector<int> arrSelectSort = baseArray;
+        // make a copy for merge sort
+        std::vector<int> arrMergeSort = baseArray;
+
+
+        timer.start();
+        selectionSort(arrSelectSort);
+        // selectionSort time in miliseconds
+        double selectTime = timer.stop();
+
+
+        timer.start();
+        mergeSort(arrMergeSort, 0, arrMergeSort.size() - 1);
+        // mergeSort time in miliseconds
+        double mergeTime = timer.stop();
+
+
+        outFile << size << "," << selectTime << "," << mergeTime << "\n";
+
+
+        std::cout << size << ", " << selectTime << ", " << mergeTime << std::endl;
+
+        if (!foundCrossover && mergeTime < selectTime) {
+
+            foundCrossover = true;
+            crossoverSize = size;
+
+            std::cout << ">>> Merge sort is faster at size " 
+                      << size << "!\n";
+            
+            break;
+        }
+
+    }
+
+    outFile.close();
+
+    
+}
+
+
